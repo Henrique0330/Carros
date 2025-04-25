2694aa73e1d2f246c3db9ba183e676e512a6828a
Henrique0330 <henriquebelboni@gmail.com>
2025-04-24 21:27:22 -0300

    feat: add a image in the body, and putting a bluer/filter in there  #4

--- Carros/src/views/telaInicial.vue
+++ Carros/src/views/telaInicial.vue
@@ -1,9 +1,21 @@
 <script setup>
+
 </script>
 
 <template>
-    <h1>Olá Mundo!!</h1>
+    <img src="/TelaFundo.jpg" alt="McQueen e sua amiga" />
 </template>
 
 <style scoped>
+    img {
+      width: 100%;
+      height: 100vh;
+      object-fit: cover;
+      position: absolute;
+      top: 0;
+      left: 0;
+      display: flex;
+      filter: brightness(0.3);
+    }
+
 </style>
