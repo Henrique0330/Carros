99cbf8b6b6ad052a01ac5181f4f56f9a5b36eabd
marcoperuzzzocmd <marcoantperuzzo@gmail.com>
2025-05-04 21:36:07 -0300

    feat: tela garagem feita

--- Carros/src/router/index.js
+++ Carros/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import TelaInicialView from '../views/TelaInicialView.vue'
 import ListaProdutosView from '@/views/ListaProdutosView.vue'
-
+import GaragemView from '@/views/GaragemView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -20,6 +20,11 @@ const router = createRouter({
       path: '/ListaProdutos',
       name: 'ListaProdutos',
       component: ListaProdutosView,
+    },
+    {
+      path: '/garagem',
+      name: 'garagem',
+      component: GaragemView
     }
   ]
 })
